f32d996f10e2c52b97fd331cd20b3009b0ab4327
DimlyLitDragon <thedimlylitdragon@gmail.com>
2026-09-20 20:03:11 -0500

    Dialougeupdate

--- Assets/Scripts/TextBoxScript.cs
+++ Assets/Scripts/TextBoxScript.cs
@@ -1,16 +1,22 @@
-using UnityEngine;
 using TMPro;
+using UnityEngine;
 
 public class TextBoxScript : MonoBehaviour
 {
-    [SerializeField] private TextMeshProUGUI subtitle;
-    [SerializeField] private TextMeshProUGUI option1;
-    [SerializeField] private TextMeshProUGUI option2;
+    [SerializeField] private GameObject DText;
+    [SerializeField] private GameObject Dbackground;
+
+    public bool IsSpeaking = false;
+    public bool wasInterrupted = false;
+    public bool doneSpeaking = false;
+    public string nexttext = "";
+
+    private bool boxopen = false;
+    private bool txtbuilding = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
     }
 
     // Update is called once per frame
